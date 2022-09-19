80663b69c9ff470a246898226955a4456b18e16f
Devon McKenzie <novithian@gmail.com>
2022-09-19 16:11:04 -0400

    Documentation and value consistency

--- script.py
+++ script.py
@@ -14,16 +14,24 @@ DATABASE: Final[str] = 'bootcamp'
 TABLE_NAME: Final[str] = 'starverse'
 USERNAME = os.environ['BOOTCAMP_USER']
 PWD = os.environ['BOOTCAMP_CREDS']
-PORT_ID: Final[str] = 5433
+PORT_ID: Final[str] = '5433'
 
 COLUMNS = ['name', 'gender', 'height', 'mass', 'birth_year']
 COLUMN_TYPES = ['VARCHAR(50)', 'VARCHAR(6)', 'VARCHAR(12)', 'VARCHAR(12)', 'VARCHAR(12)']
 COLUMN_HEADER = ''
 COLUMN_VALUES = ''
+REPLACE_UNKNOWN: bool = True
 
 CHARACTERS = []
 
-async def fetch_people(session, url):
+async def fetch_people(session, url: str):
+	""" Fetches the paginated people collection from the SWAPI API, asynchronously.
+		Populates the `CHARACTERS` global array.
+
+			Parameters:
+				session (ClientSession): Session interface for making HTTP requests
+				url (str): The paginated url to fetch from
+	"""
 	global CHARACTERS
 	async with session.get(url) as response:
 		if response.status != 200:
@@ -40,6 +48,12 @@ async def fetch_people(session, url):
 			await fetch_people(session, next_page)
 
 def build_columns():
+	""" Builds the table column insert headers, insert value pattern, and CREATE TABLE column declarations.
+		Returns the CREATE TABLE column declarations.
+
+			Returns:
+				column_decl (str): The column declarations for a CREATE TABLE statement
+	"""
 	column_decl = 'id SERIAL PRIMARY KEY,'
 	global COLUMN_HEADER
 	global COLUMN_VALUES
@@ -58,6 +72,15 @@ def build_columns():
 	return column_decl
 
 def create_table(cursor):
+	""" Creates the table with the specified `TABLE_NAME` and column declarations from `build_columns`. 
+		Returns `True` or `False`, if the table creation was successful or not.
+
+			Parameters:
+				cursor (Cursor) : Allows Python to execute PostgreSQL command in a database session
+
+			Returns:
+				`True` or `False, if the table creation was successful or not
+	"""
 	try:
 		# Delete a previous iteration of the table, if exists
 		drop_table_query = f'''
@@ -73,29 +96,50 @@ def create_table(cursor):
 
 		cursor.execute(create_table_query)
 
-		return 1
+		return True
 	except Exception as err:
 		print('An exception occurred while creating the table: ', err)
-		return 0
+		return False
 
 def populate_table(cursor):
+	""" Populates the table with the `CHARACTER` array that was populated by the `fetch_people` function. 
+		Returns `True` or `False`, if the table population was successful or not.
+
+			Parameters:
+				cursor (Cursor) : Allows Python to execute PostgreSQL command in a database session
+
+			Returns:
+				Returns `True` or `False`, if the table population was successful or not.
+	"""
 	insert_row_query = f'''INSERT INTO {TABLE_NAME} ({COLUMN_HEADER}) VALUES ({COLUMN_VALUES});'''
 
 	try:
 		for char in CHARACTERS:
 			# Edge case where the API is inconsistent
+			name = char['name']
+			height = char['height']
+			mass = char['mass']
+			birth_year = char['birth_year']
 			gender = char['gender']
+
 			if char['name'] == 'Jabba Desilijic Tiure':
 				gender = 'male'
 			else:
 				if gender == 'none':
 					gender = 'n/a'
-				
-			cursor.execute(insert_row_query, (char['name'], gender, char['height'], char['mass'], char['birth_year']))
-		return 1
+			
+			if REPLACE_UNKNOWN:
+				if height == 'unknown':
+					height = "-1"
+				if mass == 'unknown':
+					mass = "-1"
+				if birth_year == 'unknown':
+					birth_year = "-1"
+			cursor.execute(insert_row_query, (name, gender, height, mass, birth_year))
+		return True
 	except Exception as err:
 		print('An exception occurred while inserting a row into the table: ', err)
-		return 0
+		return False
 
 async def main():
 	print ('Fetching from SWAPI..')
