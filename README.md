f379d19f68696fd2b687246613d6dfc522db8ea5
Devon McKenzie <novithian@gmail.com>
2022-09-23 11:48:08 -0400

    Added import all tables, added format flag, added syntax error messages, updated documentation, updated README

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # swapi-to-postgresql
-A utility script to import `people` from Star Wars API to a local postgreSQL database
+A utility script to import data from Star Wars API to a local postgreSQL database
 ![Showcase Image](/screenshots/Capture.PNG)
 
 ## Pre-Requisites
@@ -15,13 +15,45 @@ A utility script to import `people` from Star Wars API to a local postgreSQL dat
 ## Usage
 - `HOSTNAME`: `localhost`
 - `DATABASE`: The name of the database (DEFAULT: `bootcamp`)
-- `TABLE_NAME`: The name of the table(DEFAULT: `starverse`)
 - `USERNAME`: An environment variable of the username you use for postgreSQL (DEFAULT: `BOOTCAMP_USER`)
 - `PWD`: An environment variable of the password you use for postgreSQL (DEFAULT: `BOOTCAMP_CREDS`)
 - `PORT_ID` The port postgreSQL is running on
+
+__Options__: 
+- `-c, --cache`: Set whether to take advantage of caching
+	- DEFAULT: `True`
+	- To disable: `-c=false` or `--cache=false` 
+- `-f, --force`: Forces a refetch to update the cache
+	- DEFAULT: `False`
+	- To enable: `-f` or `--force`
+- `-fmt, --format`: Set whether it should run the consistency formatter
+	- DEFAULT: `True`
+	- To disable: `-fmt=false` or `--format=false`
+- `-v, --verbose`: Produce and display a detailed output of the process
+	- DEFAULT: `False`
+	- To enable: `-v` or `--verbose`
 ``` python
+
 cd swapi-to-postgresql
 
 # In the repo source directory
 python ./script.py
+python ./script.py -c=false # Turn off the use of caching
+python ./script.py -f # Force a cache update
+python ./script.py -v # Produce a detailed output
+python ./script.py --cache=false
+python ./script.py --force
+python ./script.py --format=false # Disable consistency formatting
+python ./script.py -c=false -v -fmt=false
+python ./script.py -c=false --verbose
+python ./script.py --cache=false --verbose
 ```
+
+## Future Features
+---
+- [x] Import all tables
+- [x] Format flag
+- [x] Syntax error messages for flag setting
+- [x] Update documentation to reflect the refactor
+- [ ] Flag to use another dbname
+- [ ] Flag to use another port
--- script.py
+++ script.py
@@ -1,22 +1,19 @@
 from logging import error
 import pathlib
-from re import sub
 from typing import Final
 from pathlib import Path
 from sys import argv
 import os
 import json
-from unicodedata import category
 import psycopg2
 import asyncio
 from aiohttp import ClientSession
 
 # Database info
 HOSTNAME: Final[str] = 'localhost'
-DATABASE: Final[str] = 'bootcamp_sw'
-TABLE_NAME: Final[str] = 'people'
-USERNAME = os.environ['BOOTCAMP_USER']
-PWD = os.environ['BOOTCAMP_CREDS']
+DATABASE: Final[str] = 'bootcamp'
+USERNAME: str = os.environ['BOOTCAMP_USER']
+PWD: str = os.environ['BOOTCAMP_CREDS']
 PORT_ID: Final[str] = '5433'
 
 # Options
@@ -26,54 +23,9 @@ CACHE: bool = True
 FORCE_CACHE_UPDATE: bool = False
 
 SWAPI_API_URL: Final[str] = 'https://swapi.dev/api/'
-CATEGORY_NAMES = ['films', 'people', 'planets', 'species', 'starships', 'vehicles']
-DATA = {}
-SCHEMA = {
-	'some_table_no_key': { 
-		'header': "",
-		'value_format': "",
-		'generated_key': False,
-		'key': [],
-		'columns': [
-			{
-				'column_name': 'episode_id',
-				'column_type': 'SMALLINT NOT NULL'
-			},
-			{
-				'column_name': 'title',
-				'column_type': 'VARCHAR(25) NOT NULL'
-			},
-			
-			{
-				'column_name': 'opening_crawl',
-				'column_type': 'VARCHAR(575)'
-			}
-		]
-	},
-	'some_table': { 
-		'header': "",
-		'value_format': "",
-		'generated_key': False,
-		'key': [
-			'episode_id',
-			'title'
-		],
-		'columns': [
-			{
-				'column_name': 'episode_id',
-				'column_type': 'SMALLINT NOT NULL'
-			},
-			{
-				'column_name': 'title',
-				'column_type': 'VARCHAR(25) NOT NULL'
-			},
-			
-			{
-				'column_name': 'opening_crawl',
-				'column_type': 'VARCHAR(575)'
-			}
-		]
-	},
+CATEGORY_NAMES: list = ['films', 'people', 'planets', 'species', 'starships', 'vehicles']
+DATA: dict = {}
+SCHEMA: dict = {
 	'films': { 
 		'header': "",
 		'value_format': "",
@@ -158,13 +110,242 @@ SCHEMA = {
 				'column_type': 'VARCHAR(50)'
 			}
 		]
-	}
+	},
+	'planets': { 
+		'header': '',
+		'value_format': '',
+		'generated_key': True,
+		'key': [
+			'id'
+		],
+		'columns': [
+			{
+				'column_name': 'id',
+				'column_type': 'SERIAL'
+			},
+			{
+				'column_name': 'name',
+				'column_type': 'VARCHAR(50)'
+			},
+			{
+				'column_name': 'rotation_period',
+				'column_type': 'VARCHAR(18)'
+			},
+			{
+				'column_name': 'orbital_period',
+				'column_type': 'VARCHAR(18)'
+			},
+			{
+				'column_name': 'diameter',
+				'column_type': 'VARCHAR(12)'
+			},
+			{
+				'column_name': 'climate',
+				'column_type': 'VARCHAR(30)'
+			},
+			{
+				'column_name': 'gravity',
+				'column_type': 'VARCHAR(45)'
+			},
+			{
+				'column_name': 'terrain',
+				'column_type': 'VARCHAR(45)'
+			},
+			{
+				'column_name': 'surface_water',
+				'column_type': 'VARCHAR(15)'
+			},
+			{
+				'column_name': 'population',
+				'column_type': 'VARCHAR(15)'
+			}
+		]
+	},
+	'species': { 
+		'header': '',
+		'value_format': '',
+		'generated_key': True,
+		'key': [
+			'id'
+		],
+		'columns': [
+			{
+				'column_name': 'id',
+				'column_type': 'SERIAL'
+			},
+			{
+				'column_name': 'name',
+				'column_type': 'VARCHAR(25)'
+			},
+			{
+				'column_name': 'classification',
+				'column_type': 'VARCHAR(15)'
+			},
+			{
+				'column_name': 'average_height',
+				'column_type': 'VARCHAR(10)'
+			},
+			{
+				'column_name': 'skin_colors',
+				'column_type': 'VARCHAR(50)'
+			},
+			{
+				'column_name': 'hair_colors',
+				'column_type': 'VARCHAR(50)'
+			},
+			{
+				'column_name': 'eye_colors',
+				'column_type': 'VARCHAR(50)'
+			},
+			{
+				'column_name': 'average_lifespan',
+				'column_type': 'VARCHAR(12)'
+			},
+			{
+				'column_name': 'homeworld',
+				'column_type': 'VARCHAR(50)'
+			},
+			{
+				'column_name': 'language',
+				'column_type': 'VARCHAR(15)'
+			}
+		]
+	},
+	'starships': { 
+		'header': '',
+		'value_format': '',
+		'generated_key': True,
+		'key': [
+			'id'
+		],
+		'columns': [
+			{
+				'column_name': 'id',
+				'column_type': 'SERIAL'
+			},
+			{
+				'column_name': 'name',
+				'column_type': 'VARCHAR(32)'
+			},
+			{
+				'column_name': 'model',
+				'column_type': 'VARCHAR(50)'
+			},
+			{
+				'column_name': 'manufacturer',
+				'column_type': 'VARCHAR(80)'
+			},
+			{
+				'column_name': 'cost_in_credits',
+				'column_type': 'VARCHAR(15)'
+			},
+			{
+				'column_name': 'length',
+				'column_type': 'VARCHAR(12)'
+			},
+			{
+				'column_name': 'max_atmosphering_speed',
+				'column_type': 'VARCHAR(12)'
+			},
+			{
+				'column_name': 'crew',
+				'column_type': 'VARCHAR(12)'
+			},
+			{
+				'column_name': 'passengers',
+				'column_type': 'VARCHAR(12)'
+			},
+			{
+				'column_name': 'cargo_capacity',
+				'column_type': 'VARCHAR(15)'
+			},
+			{
+				'column_name': 'consumables',
+				'column_type': 'VARCHAR(12)'
+			},
+			{
+				'column_name': 'hyperdrive_rating',
+				'column_type': 'VARCHAR(10)'
+			},
+			{
+				'column_name': 'MGLT',
+				'column_type': 'VARCHAR(8)'
+			},
+			{
+				'column_name': 'starship_class',
+				'column_type': 'VARCHAR(35)'
+			}
+		]
+	},
+	'vehicles': { 
+		'header': '',
+		'value_format': '',
+		'generated_key': True,
+		'key': [
+			'id'
+		],
+		'columns': [
+			{
+				'column_name': 'id',
+				'column_type': 'SERIAL'
+			},
+			{
+				'column_name': 'name',
+				'column_type': 'VARCHAR(32)'
+			},
+			{
+				'column_name': 'model',
+				'column_type': 'VARCHAR(50)'
+			},
+			{
+				'column_name': 'manufacturer',
+				'column_type': 'VARCHAR(80)'
+			},
+			{
+				'column_name': 'cost_in_credits',
+				'column_type': 'VARCHAR(15)'
+			},
+			{
+				'column_name': 'length',
+				'column_type': 'VARCHAR(12)'
+			},
+			{
+				'column_name': 'max_atmosphering_speed',
+				'column_type': 'VARCHAR(12)'
+			},
+			{
+				'column_name': 'crew',
+				'column_type': 'VARCHAR(12)'
+			},
+			{
+				'column_name': 'passengers',
+				'column_type': 'VARCHAR(12)'
+			},
+			{
+				'column_name': 'cargo_capacity',
+				'column_type': 'VARCHAR(15)'
+			},
+			{
+				'column_name': 'consumables',
+				'column_type': 'VARCHAR(15)'
+			},
+			{
+				'column_name': 'vehicle_class',
+				'column_type': 'VARCHAR(35)'
+			}
+		]
+	},
 }
 
+async def fetch_category(session: ClientSession, category_name: str):
+	""" Fetches the category data, from the SWAPI endpoint of name `category_name`, and 
+		appends it to the DATA dictionary.
 
-
-async def fetch_category(session, category_name: str):
-	catergory_entries = []
+		Parameters:
+			session (ClientSession): The app's Psycopg2 session
+			category_name (str): The name of the category you wish to fetch
+	"""
+	catergory_entries: list = []
 
 	await fetch_page(session, SWAPI_API_URL + category_name + '/', catergory_entries)
 
@@ -173,7 +354,16 @@ async def fetch_category(session, category_name: str):
 	if VERBOSE:
 		print(DATA[category_name])
 
-async def fetch_page(session: ClientSession, url: str, container):
+async def fetch_page(session: ClientSession, url: str, container: list):
+	""" Fetches a page of data for a particular category. If there is another page, then 
+		it'll automatically recursively call `fetch_page()` until all the pages are collected
+		and appended to the passed container.
+		
+
+		Parameters:
+			session (ClientSession): The app's Psycopg2 session
+			container (str): The container to append the new page to
+	"""
 	async with session.get(url) as response:
 		if response.status != 200:
 			error("Encountered a problem: %s", response.status)
@@ -189,6 +379,13 @@ async def fetch_page(session: ClientSession, url: str, container):
 			await fetch_page(session, next_page, container)
 
 def write_to_file(filename: str, path: str):
+	""" Caches the fetched data at path/filename.json. If the path does not exist, it will be created.
+
+			Parameters:
+				filename (str): The root name of the exported file (DEFAULT: `swapi_data`)
+				path (str): The path to the directory of the exported file (DEFAULT: `./bin/`)
+	"""
+
 	if not Path(path).exists():
 		pathlib.Path(path).mkdir(parents=True, exist_ok=True)
 
@@ -196,13 +393,23 @@ def write_to_file(filename: str, path: str):
 		json.dump(DATA, target_file, indent=4)
 
 def read_from_file(filename: str, path: str):
+	""" Reads the cached data at path/filename.json. 
+
+			Parameters:
+				filename (str): The root name of the exported file (DEFAULT: `swapi_data`)
+				path (str): The path to the directory of the exported file (DEFAULT: `./bin/`)
+	"""
 	global DATA
 	with open(path + '/' + filename + '.json') as target_file:
 		DATA = json.load(target_file)
 
-def build_columns(category_name):
-	""" Builds the table column insert headers, insert value pattern, and CREATE TABLE column declarations.
+def build_columns(category_name) -> str:
+	""" Builds the table column insert headers, insert value pattern, and CREATE TABLE column declarations
+		for the table for `category_name`.
 		Returns the CREATE TABLE column declarations.
+
+			Parameters:
+				category_name (str): The name of the category to build to columns for
 
 			Returns:
 				column_decl (str): The column declarations for a CREATE TABLE statement
@@ -212,19 +419,19 @@ def build_columns(category_name):
 	generated_key: bool = SCHEMA[category_name]['generated_key']
 	key: list = SCHEMA[category_name]['key']
 	has_primary_key: bool = True if len(key) > 0 else False
-	column_types = SCHEMA[category_name]['columns']
-	column_decl = ''
-	column_header = ''
-	column_format = ''
-	value = '%s'
-	type_count = len(column_types)
+	column_types: list = SCHEMA[category_name]['columns']
+	column_decl: str = ''
+	column_header: str = ''
+	column_format: str = ''
+	value: str = '%s'
+	type_count: int = len(column_types)
 
 	for index, col in enumerate(column_types):
-		name = col['column_name']
-		column_type = col['column_type']
+		name: str = col['column_name']
+		column_type: str = col['column_type']
 		column_decl += ' ' + name + ' ' + column_type
 		
-		not_end = index + 1 < type_count
+		not_end: bool = index + 1 < type_count
 
 		if not_end or has_primary_key:
 			column_decl += ','
@@ -249,25 +456,26 @@ def build_columns(category_name):
 
 	return column_decl
 
-def create_table(cursor, table_name):
-	""" Creates the table with the specified `TABLE_NAME` and column declarations from `build_columns`. 
+def create_table(cursor, table_name) -> bool:
+	""" Creates the table with the specified `table_name` and column declarations from `build_columns`. 
 		Returns `True` or `False`, if the table creation was successful or not.
 
 			Parameters:
-				cursor (Cursor) : Allows Python to execute PostgreSQL command in a database session
+				cursor (Cursor): Allows Python to execute PostgreSQL command in a database session
+				table_name (str): The name of the table, typically the same as the category name
 
 			Returns:
 				`True` or `False, if the table creation was successful or not
 	"""
 	try:
 		# Delete a previous iteration of the table, if exists
-		drop_table_query = f'''
+		drop_table_query: str = f'''
 		DROP TABLE IF EXISTS {table_name};
 		'''
 		cursor.execute(drop_table_query)
 
 
-		column_decls = build_columns(table_name)
+		column_decls: str = build_columns(table_name)
 		comp_key: str = 'PRIMARY KEY ('
 
 		key: list = SCHEMA[table_name]['key']
@@ -286,7 +494,7 @@ def create_table(cursor, table_name):
 		elif sub_key_count == 0:
 			comp_key = ''
 
-		create_table_query = f'''CREATE TABLE {table_name} (
+		create_table_query: str = f'''CREATE TABLE {table_name} (
 			{column_decls}
 			{comp_key}
 		);'''
@@ -294,94 +502,111 @@ def create_table(cursor, table_name):
 		cursor.execute(create_table_query)
 
 		return True
+
 	except Exception as err:
 		print('An exception occurred while creating the table: ', err)
 		return False
 
-def populate_table(cursor, category_name):
-	""" Populates the table with the `CHARACTER` array that was populated by the `fetch_people` function. 
+def populate_table(cursor, category_name) -> bool:
+	""" Populates the table with the name that matches `category_name` with the fetched or cached data.
 		Returns `True` or `False`, if the table population was successful or not.
 
 			Parameters:
-				cursor (Cursor) : Allows Python to execute PostgreSQL command in a database session
+				cursor (Cursor): Allows Python to execute PostgreSQL command in a database session
+				category_name (str): The name of the table to populate
 
 			Returns:
 				Returns `True` or `False`, if the table population was successful or not.
 	"""
-	schema = SCHEMA[category_name]
-	collection = DATA[category_name]
+	schema:dict = SCHEMA[category_name]
+	collection: list = DATA[category_name]
 	has_generated_key: bool = schema['generated_key']
 	key: list = schema['key']
-	insert_row_query = f'''INSERT INTO {category_name} ({schema['header']}) VALUES ({schema['value_format']});'''
-	unknown = 'unknown'
-	none = 'none'
-	na = 'n/a'
+	insert_row_query: str = f'''INSERT INTO {category_name} ({schema['header']}) VALUES ({schema['value_format']});'''
+
+	# Formatting
+	unknown: str = 'unknown'
+	none: str = 'none'
+	na: str = 'n/a'
+	upper_na: str = 'N/A'
 
 	try:
 		for entry in collection:
-			data = ()
+			data: tuple = ()
+
 			for column in schema['columns']:
-				column_name = column['column_name']
+				column_name: str = column['column_name']
+
 				if has_generated_key and column_name in key:
 					continue
+
 				entry_data = entry[column_name]
 
 			# Make the columns with 'unknown's consistent
 				if FORMAT:
 					if type(entry_data).__name__ == "str":
-						if entry_data == unknown or entry_data == none:
+						if entry_data == unknown or entry_data == none or entry_data == upper_na:
 							entry_data = na
 				data += (entry_data,)
 
 			cursor.execute(insert_row_query, data)
 		return True
+
 	except Exception as err:
 		print('An exception occurred while inserting a row into the table: ', err)
 		return False
 
 def print_help():
-	""" Prints out the help menu to the console. """
+	""" Prints out the help menu to the terminal. """
 
 	print('Usage: python ./script.py ([options])')
 	print('Example:')
-	print('\tpython ./script.py -c')
+	print('\tpython ./script.py -c=false')
 	print('\tpython ./script.py -f')
-	print('\tpython ./script.py --cache')
+	print('\tpython ./script.py --cache=false')
 	print('\tpython ./script.py --force')
-	print('\tpython ./script.py -c -v')
-	print('\tpython ./script.py -c --verbose')
-	print('\tpython ./script.py --cache --verbose')
+	print('\tpython ./script.py --format=false')
+	print('\tpython ./script.py -c=false -v -fmt=false')
+	print('\tpython ./script.py -c=false --verbose')
+	print('\tpython ./script.py --cache=false --verbose')
 	print('\n')
 	print('Options:')
 	print('-c, --cache\t\tWhether or not to cache and use the cached results. Default=true')
-	print('\t\t\tDisable caching and use of any existing cache:')
+	print('\t\t\tDisable caching and the use of any existing cache:')
 	print('\t\t\t-c=false')
-	print('\t\t\t--c=false')
-	print("-f, --force\t")
+	print('\t\t\t--cache=false')
+	print('-fmt, --format\t\tFormat the fields to be consistent (DEFAULT=true)')
+	print('\t\t\t-fmt=false')
+	print('\t\t\t--format=false')
+	print('\t\t\tDisable consistency formatting:')
+	print("-f, --force\t\tForce a cache update")
 	print('-v, --verbose\t\tPrints out detailed information of the process')
 	print('')
 	print('General Options: ')
 	print('-h, --help\t\tPrint usage information')
 
 async def main():
-	global VERBOSE, FORCE_CACHE_UPDATE, CACHE
-	filename = 'swapi_data'
-	bin_location = './bin'
+	global VERBOSE, FORCE_CACHE_UPDATE, CACHE, FORMAT
+	filename: str = 'swapi_data'
+	bin_location: str = './bin'
 
 	# Handle args
 	if '-h' in argv or '--help' in argv:
 		print_help()
+
 		return
 
 	arg_len: int = len(argv)
 
 	if arg_len > 1:
 		for i, arg in enumerate(argv):
-			la = arg.lower()
+			la: str = arg.lower()
+
 			if '-v' == la or '--verbose' == la:
 				VERBOSE = True
+
 			elif la.startswith('-c=') or la.startswith('--cache='):
-				count: int = la.count('=');
+				count: int = la.count('=')
 				
 				if count == 1:
 					index: int = la.index('=') + 1
@@ -389,18 +614,56 @@ async def main():
 
 					if (value == 'false' or value == 'f'):
 						CACHE = False
+
 					elif (value == 'true' or value == 't'):
 						print('Redundant cache flag as caching is on by default')
+
+					else:
+						print('Invalid syntax: %s is not a valid option. Options: true/TRUE/t, false/FALSE/f')
+						return
+
 				elif count > 1:
 					print('Invalid syntax: Too many = in the flag %s', la)
 					return
+
 			elif '-f' == la or '--force' == la:
 				FORCE_CACHE_UPDATE = True
+
+			elif la.startswith('-fmt=') or la.startswith('--format='):
+				count: int = la.count('=')
+
+				if count == 1:
+					index: int = la.index('=') + 1
+					value: str = la[index:].lower()
+
+					if value == 'false' or value == 'f':
+						FORMAT = False
+
+					elif (value == 'true' or value == 't'):
+						print('Redundant format flag as caching is on by default')
+
+					else:
+						print('Invalid syntax: %s is not a valid option. Options: true/TRUE/t, false/FALSE/f')
+						return
+
+
+				elif count > 1:
+					print('Invalid syntax: Too many = in the flag %s', la)
+					return
+
 
 	if FORCE_CACHE_UPDATE and not CACHE:
 		print('Cannot force a cache update and not have caching enabled.')
 		print('Please remove the flag to disable caching if you desire to update the existing cache.')
+
 		return
+
+	if VERBOSE:
+		print('Running with Options:')
+		print('\t Verbose Output:', VERBOSE)
+		print('\t Formatting:', FORMAT)
+		print('\t Cache:', CACHE)
+		print('\t Force Cache Update:', FORCE_CACHE_UPDATE)
 
 	cache_exists: bool = Path(bin_location + '/' + filename + '.json').exists()
 
@@ -414,6 +677,7 @@ async def main():
 	else:
 		if CACHE and not cache_exists and not FORCE_CACHE_UPDATE:
 			print("No cache to read from..")
+
 		elif FORCE_CACHE_UPDATE:
 			print("Forcing cache update..")
 
@@ -432,47 +696,45 @@ async def main():
 
 	try:
 		print(f'Connecting to postgresql database at: {HOSTNAME}:{PORT_ID}/{DATABASE}')
+
 		connection = psycopg2.connect(
 			host = HOSTNAME,
 			dbname = DATABASE,
 			user = USERNAME,
 			password = PWD,
 			port = PORT_ID)
-
 		cursor = connection.cursor()
 
-		print ('Creating table..')
-		if not create_table(cursor, 'people'):
-			quit(-1)
+		print ('Constructing tables..')
 
-		print ('Populating table..')
-		if not populate_table(cursor, 'people'):
-			quit(-1)
+		for category in SCHEMA:
+			if VERBOSE:
+				print ('Constructing %s table..', category)
+			
+			if not create_table(cursor, category):
+				quit(-1)
 
-		# print('\n')
-		# if not create_table(cursor, 'films'):
-		# 	quit(-1)
-		# print('\n')
-		# if not create_table(cursor, 'some_table'):
-		# 	quit(-1)
-		# print('\n')
-		# if not create_table(cursor, 'some_table_no_key'):
-		# 	quit(-1)
+		print ('Populating tables..')
 
+		for category in SCHEMA:
+			if VERBOSE:
+				print ('Populating %s table..', category)
 
-		# print ('Populating table..')
-		# if not populate_table(cursor, SCHEMA['some_table']):
-		# 	quit(-1)
+			if not populate_table(cursor, category):
+				quit(-1)
 
-		# print(len(DATA['people']), ' records inserted')
+			if VERBOSE:	
+				print(len(DATA[category]), ' records inserted')
 
 		connection.commit()
 		
 	except Exception as err:
 		print('Encountered an error: ', err.__class__.__name__)
+
 	finally:
 		if cursor is not None:
 			cursor.close()
+
 		if connection is not None:
 			connection.close()
 
